3330e83eb78d240ec180a81e0307a2e0f580b4fd
DonTrevi <valentin.trevi@gmail.com>
2025-06-19 15:54:02 -0400

    Programa vainilla

---
+++ Programa_2_Calcular_el_peso_ideal.py
@@ -0,0 +1,18 @@
+#Calcular el peso ideal
+print("Este programa determina su peso ideal y le dice si está sobrepeso")
+print("Ingrese su peso actual ")
+peso_actual = int(input())
+print("Ingrese su altura actual ")
+altura = float(input())
+imc = peso_actual/(altura**2)
+print("Su IMC es de... ")
+print(imc)
+
+if imc < 18.5:
+	print("Su peso está bajo...")
+if imc < 24.9 and imc > 18.5:
+	print("Su peso es normal...")
+if imc < 29.9 and imc > 25.0:
+	print("Está en sobrepeso...")
+if imc > 30:
+	print("Usted sufre de obesidad...")
---
+++ README.md
@@ -0,0 +1,2 @@
+# peso_ideal
+Este es un programa sencillo escrito en Python que toma la altura y el peso y determina si estás saludable u obeso
